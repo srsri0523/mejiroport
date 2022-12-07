8cedfb2e3b88d0d4fdaf7cf57944c6ac058b7842
松本凌雅 <clione@ip-10-79-13-237.ap-northeast-1.compute.internal>
2022-12-07 13:45:06 +0900

    first commit

---
+++ android/app/src/main/kotlin/mejiro/xx/mejiro/MainActivity.kt
@@ -0,0 +1,6 @@
+package mejiro.xx.mejiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/mejiro_xx/mejiro/MainActivity.kt
@@ -0,0 +1,6 @@
+package mejiro_xx.mejiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
